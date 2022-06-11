682069fb8188e4decfe3ef05b8a0fbeee3075fcb
lothing <407801189@qq.com>
2022-06-11 08:36:27 +0800

    update site.url

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,7 +5,7 @@
 SimpleJekyllSearch({
     searchInput: document.getElementById('search-input'),
     resultsContainer: document.getElementById('results-container'),
-    json: base_url + '/search.json',
+    json: '/search.json',
     searchResultTemplate: '<li><a href="{url}" title="{title}">{title}</a></li>',
     noResultsText: '<li class="no-results">No results found :(</li>',
     limit: 10,
